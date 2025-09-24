e3c432668ceb4bcb4869ec77b399e64f8b5344c6
iroh <macedocesar.tiago@gmail.com>
2025-09-24 14:29:58 -0300

    constantes simbólicas

--- LinguagemC/cap1/exercicio_5.c
+++ LinguagemC/cap1/exercicio_5.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 
+#define INICIO 300 
+#define FIM 0 
+#define INCREMENTO 20 
+
 int main() 
 {
     int fahrenheit;
 
-    for (fahrenheit = 300; fahrenheit >= 0; fahrenheit -= 20) 
+    for (fahrenheit = INICIO; fahrenheit >= FIM; fahrenheit -= INCREMENTO) 
     {   
         printf("%3d\t%6.1f\n", fahrenheit, ((5.0/9.0) * (fahrenheit - 32)));
     }
